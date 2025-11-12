81a143f2611be52fd62ee4ece9dc37809c6d7c7b
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2025-11-12 10:47:12 +0100

    crear funcion salir

--- sprint4tkinter/controller/app_controller.py
+++ sprint4tkinter/controller/app_controller.py
@@ -1,9 +1,12 @@
 import customtkinter as ctk
 from view.main_view import app, scrollable, panel
 
-class Controlador:
+class Controller:
     def __init__(self, app, scrollable, panel):
         self.app = app
         self.scrollable = scrollable
         self.panel = panel
         self.usuarios = []
+
+def salir(self):
+        self.app.destroy()
