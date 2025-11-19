a8bb9d7ba37f1a23781743657ca499f485beb941
Breixo Gonzalez <bgonzalezp24@fpcoruna.afundacion.org>
2025-11-19 10:48:29 +0100

    crear metodo guardarcsv con ruta dentro de carpeta proyecto

--- sprint4tkinter/model/usuario_model.py
+++ sprint4tkinter/model/usuario_model.py
@@ -11,4 +11,19 @@ class Usuario:
     def __str__(self):
         return f"Usuario({self.nombre}, {self.edad} años, {self.genero})"
 
-    #def guardar_csv(self):
+    def guardar_csv(self,ruta_archivo=None):#con none se guarda en la carpeta del proyecto
+            """
+            Guarda una lista de objetos Usuario en un archivo CSV.
+            usuarios: lista de objetos Usuario
+            ruta_archivo: path del CSV donde se guardarán los datos
+            """
+            # Abrimos el archivo en modo escritura
+            with open(ruta_archivo, "w", newline="", encoding="utf-8") as archivo:
+                escritor = csv.writer(archivo)
+
+                # Escribimos la cabecera
+                escritor.writerow(["nombre", "edad", "genero", "avatar"])
+
+                # Escribimos los datos de cada usuario
+                for usuario in usuarios:
+                    escritor.writerow([usuario.nombre, usuario.edad, usuario.genero, usuario.avatar])
